fff283aa221ce7ab361e502bf074d73ac920e425
Arya Medhika Abdillah <mdkarya010@gmail.com>
2026-05-14 19:02:11 +0700

    Update main.js

--- home/main.js
+++ home/main.js
@@ -7,8 +7,8 @@
 // ═══════════════════════════════
 
 const provinces = [
-  { name: "Aceh",                   icon: "./assets/provinsi/aceh.svg" },
-  { name: "Sumatera Utara",         icon: "../assets/provinsi/sumut.svg" },
+  { name: "Aceh",                   icon: "assets/provinsi/aceh.svg" },
+  { name: "Sumatera Utara",         icon: "assets/provinsi/sumut.svg" },
   { name: "Sumatera Barat",         icon: "../../assets/provinsi/sumbar.svg" },
   { name: "Riau",                   icon: "../../../assets/provinsi/riau.svg" },
   { name: "Kepulauan Riau",         icon: "/assets/provinsi/kepri.svg" },
